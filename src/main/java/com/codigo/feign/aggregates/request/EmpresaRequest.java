db21d17db3fdec1e3f68449585749c17739f21d0
renzo-cr@hotmail.com <renzo-cr@hotmail.com>
2024-02-11 12:35:39 -0500

    Actualizado

---
+++ src/main/java/com/codigo/feign/aggregates/request/EmpresaRequest.java
@@ -0,0 +1,11 @@
+package com.codigo.feign.aggregates.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmpresaRequest {
+    public String tipoDoc;
+    public String numDoc;
+}
